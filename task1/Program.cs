d486848368389a1f3a9e42bd7146ba82efede4f5
GZotov <gzotovqq@gmail.com>
2023-12-25 22:36:47 +0300

    Task1 done

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+
+int number = 322;
+
+if (number % 7 == 0 && number % 23 == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
