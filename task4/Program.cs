e553691e6a83608e466a02cefe63c5a3dbb59592
GZotov <gzotovqq@gmail.com>
2023-12-25 23:54:19 +0300

    task4 done?

--- task4/Program.cs
+++ task4/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает натуральное число N, а на выходе показывает его цифры через запятую.
+// не используя строки, но в обратном порядке
+
+int N = 12345;
+
+int r = 5; // Введите количество цифр в числе N
+
+int i = 1;
+int sum = N;
+if (N >= 1)
+{
+    while (i <= r)
+    {
+        if (i == r)
+        {
+            sum = N % 10;
+            Console.Write(sum);
+            break;
+        }
+        else{
+        sum = N % 10;
+        Console.Write($"{sum},");
+        N = N / 10;
+        i += 1;
+        }
+    }
+}
+else
+{
+    Console.WriteLine($"Число {N} не является натуральным");
+}
