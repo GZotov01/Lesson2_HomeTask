7b6ad9e17a036870fac816c098c54e0d82e0e516
GZotov <gzotovqq@gmail.com>
2023-12-25 22:57:46 +0300

    task3 done

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход целое число из отрезка [10, 99] и показывает наибольшую цифру числа.
+int number = 96;
+int n1, n2;
+
+if (number >= 10 && number <= 99)
+{
+    n1 = number / 10;
+    n2 = number % 10;
+
+    if (n1 > n2)
+    {
+        Console.WriteLine(n1);
+    }
+    else
+    {
+        Console.WriteLine(n2);
+    }
+}
+else
+{
+    Console.WriteLine("Out of range");
+}
